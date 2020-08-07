b4631a62c028643eaa268b10a10b48fc74677bb3
hadiwinoto <inimulyohadiwinoto@gmail.com>
2020-08-07 17:08:05 +0700

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,56 @@
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
+require('./bootstrap');
+
+window.Vue = require('vue');
+
+import { Form, HasError, AlertError } from 'vform'
+
+window.Form = Form;
+Vue.component(HasError.name, HasError)
+Vue.component(AlertError.name, AlertError)
+
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+let routes = [
+    { path: '/dashboard', component: require('./components/Dashboard.vue').default  }
+  ]
+
+
+const router = new VueRouter({
+    mode: 'history',
+    routes // short for `routes: routes`
+  })
+
+
+/**
+ * The following block of code may be used to automatically register your
+ * Vue components. It will recursively scan this directory for the Vue
+ * components and automatically register them with their "basename".
+ *
+ * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
+ */
+
+// const files = require.context('./', true, /\.vue$/i)
+// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('dashboard-page', require('./components/Dashboard.vue').default);
+Vue.component('subheader-dashboard', require('./components/SubheaderDashboard.vue').default);
+
+/**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+
+const app = new Vue({
+    el: '#app',
+    router
+});
+
---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('subheader')
+<subheader-dashboard></subheader-dashboard>
+@endsection
+@section('content')
+<dashboard-page></dashboard-page>
+@endsection
