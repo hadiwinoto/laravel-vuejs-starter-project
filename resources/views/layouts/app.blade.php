5ff330dfcdcb54bdb182755d3b3dc83e357f5fa5
hadiwinoto <inimulyohadiwinoto@gmail.com>
2020-08-07 17:50:57 +0700

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,9 +23,12 @@
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <span>
+                LARAVUE
+        </span>
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                   LaraVue
+                <a href="{{ url('/home') }}">
+                   Dashboard
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
